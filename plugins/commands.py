fbf649955e5543fb090f77c03a3570a7ff6babe8
Devilriya <logesh.12a3@gmail.com>
2022-12-21 21:17:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,7 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('JOIN NOW', url='https://t.me/shapeofdindiguldiscussion') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('JOIN NOW', url='https://t.me/Teamisaimini') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
